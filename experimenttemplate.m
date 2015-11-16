fc1ca91be80f2345fccb2dcf291ec699cc829c06
nikolopo <nikolopo@iti.gr>
2015-11-16 17:34:27 +0200

    "added the experimenttemplate.m file"

---
+++ experimenttemplate.m
@@ -0,0 +1,23 @@
+% load filtMAMEM
+% session = ssveptoolkit.util.Session(Hhp);
+% session.loadAll;
+
+transformer = ssveptoolkit.transformer.PWelchTransformer;
+
+classifier = ssveptoolkit.classifier.LIBSVMClassifier;
+
+experiment = ssveptoolkit.experiment.Experimenter;
+experiment.session = session;
+experiment.transformer = transformer;
+experiment.classifier = classifier;
+experiment.evalMethod = experiment.EVAL_METHOD_LOSO;
+experiment.run;
+accuracies = [];
+for i=1:length(experiment.results)
+    accuracies = [accuracies experiment.results{i}.getAccuracy()];
+end
+accuracies'
+%mean accuracy for all subjects
+fprintf('mean acc = %f\n', mean(accuracies));
+%get the configuration used (for reporting)
+experiment.getExperimentInfo
