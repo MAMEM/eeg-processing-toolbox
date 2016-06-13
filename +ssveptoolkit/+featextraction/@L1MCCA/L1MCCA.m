fd9f1fe4adb8a3c7882f41d9769baa3dee518f73
George Liaros <liarosge@gmail.com>
2016-06-13 16:49:38 +0300

    added a filter for epoc data

---
+++ +ssveptoolkit/+featextraction/@L1MCCA/L1MCCA.m
@@ -0,0 +1,27 @@
+classdef L1MCCA < ssveptoolkit.featextraction.PSDExtractionBase%FeatureExtractionBase
+    %
+    % works only on our datasets, for other datasets needs modifications
+    %
+    properties (Access = public)
+        channel;
+        avgTime;
+        stimulus_freqs;
+        FreqSamp;
+        NumHarm;
+    end
+    methods (Access = public)
+        function L1MCCA = L1MCCA()
+        end
+        
+        function extract(L1MCCA)
+            L1MCCA.instanceSet = ssveptoolkit.util.L1MCCAInstanceSet(L1MCCA.trials);
+        end
+        
+        function configInfo = getConfigInfo(L1MCCA)
+            configInfo = sprintf('L1MCCA');
+        end
+        function time = getTime(L1MCCA)
+            time = 0;
+        end
+    end
+end
